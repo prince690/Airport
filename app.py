5ba14e0b4a886658908344ab9a6ff6ec0d5c8650
Prince Pandey <ptks9930@gmail.com>
2024-12-27 20:43:38 +0530

    Data ingestion

--- app.py
+++ app.py
@@ -1,4 +1,12 @@
 from src.Airplane_sensor.logger import logging
-
+from src.Airplane_sensor.exception import CustomException
+from src.Airplane_sensor.component.data_injection import DataIngestion
+import sys
 if __name__=="__main__":
+    try:
+        data_injetion=DataIngestion()
+        data_injetion.initiate_data_ingestion()
+        
+    except Exception as e:
+        raise CustomException(e,sys)
     logging.info("to check if it's working or not")
