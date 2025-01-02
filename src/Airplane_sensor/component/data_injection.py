d78a543fe8dec11759e53a7a4ea605f74762157c
Prince Pandey <ptks9930@gmail.com>
2025-01-02 10:05:37 +0530

    python notebook

--- src/Airplane_sensor/component/data_injection.py
+++ src/Airplane_sensor/component/data_injection.py
@@ -38,6 +38,8 @@ class DataIngestion:
             return(
                 self.ingestion_config.train_data_path,
                 self.ingestion_config.test_data_path
+                
+
 
 
             )
